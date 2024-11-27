918c538f9af8daf8e4dd207a2499b9e38ed1ee76
rwhitney1997 <rwhitney@confederationcollege.ca>
2024-11-27 15:24:27 -0500

    Added remove functionality

--- src/routes/Todo.svelte
+++ src/routes/Todo.svelte
@@ -9,12 +9,22 @@ function addItem(event) {
      if (todoItem == '') { //refuses to take submit when field is empty
           return;
      }
-     todoList.push(todoItem);
+     todoList = [...todoList, {
+          text: todoItem,
+          done: false
+     }];
      todoItem = '';
 }
+function removeItem(index){
+     todoList = todoList.toSpliced(index, 1);
+}
+
+function nuke (){
+     todoList = [];
+}
+
 $inspect(todoList);
 </script>
-
 
 <form onsubmit={addItem}>
 <input type="text" bind:value={todoItem}>
@@ -22,12 +32,26 @@ $inspect(todoList);
 </form>
 
 <ul>
-     {#each todoList as item}
-          <li>{item}</li>
+     {#each todoList as item, index}
+          <li>
+               <input type="checkbox" bind:checked={item.done}>
+               <span class:done={item.done}>{item.text}</span> <!-- span directive adds class when item is done, specifically - see style below -->
+               <button type="button" onclick={() => removeItem(index)} >X</button>
+          </li>
      {/each}
 </ul>
 
+{#if (todoList.length > 0)}
+<button type="button" onclick={nuke}>Nuke</button>
+{/if}
 
 <style>
-
+     ul{
+          list-style: none;
+     }
+     span.done{
+          color: rgb(137, 137, 137);
+          text-decoration: line-through;
+     }
+     /* Styles added here will only effect this component, use style.css for global */
 </style>
